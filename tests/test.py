481fd7903f3af6b7fbe84d4c331238009b14c90c
AndreasX42 <andreas.pangerl.42@gmail.com>
2023-10-09 23:05:23 +0200

    Corrected test.

--- tests/test.py
+++ tests/test.py
@@ -3,7 +3,7 @@ import unittest
 
 class IntegerArithmeticTestCase(unittest.TestCase):
     def testAdd(self):  # test method names begin with 'test'
-        self.assertEqual((1 + 2), 5)
+        self.assertEqual((1 + 2), 3)
         self.assertEqual(0 + 1, 1)
 
     def testMultiply(self):
